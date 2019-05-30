e2ce38b63958885b24fea46f601dc7a16f0a7fff
vambaster da Costa <vambaster.dacosta@hotmail.com>
2019-05-30 11:49:12 -0300

    Calculadora de aliquotas

--- lib/main.dart
+++ lib/main.dart
@@ -12,121 +12,152 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  TextEditingController weightController = TextEditingController();
-  TextEditingController heightController = TextEditingController();
+  TextEditingController rendaControler = TextEditingController();
+
+  String _infoText = "Informe sua renda";
+  String _percet = "";
 
   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-  String _infoText = "Informe seus dados!";
-
-  void _resetFields(){
-    weightController.text = "";
-    heightController.text = "";
+  void _resetFild() {
+    rendaControler.text = "";
     setState(() {
-      _infoText = "Informe seus dados!";
+      _infoText = "Informe sua renda";
+      _percet ="";
     });
-
   }
 
-  void _calculate(){
+  double _calcularValorIntegraldaFaixa(double renda, double faixa) {
+    return renda * faixa / 100;
+  }
+
+  double _calcularValorParcialdaFaixa(
+      double renda, double limite, double faixa) {
+    return (renda - limite) * faixa / 100;
+  }
+
+  void _calcular() {
     setState(() {
-      double weight = double.parse(weightController.text);
-      double height = double.parse(heightController.text) / 100;
-      double imc = weight / (height * height);
-      if(imc < 18.6){
-        _infoText = "Abaixo do Peso (${imc.toStringAsPrecision(4)})";
-      } else if(imc >= 18.6 && imc < 24.9){
-        _infoText = "Peso Ideal (${imc.toStringAsPrecision(4)})";
-      } else if(imc >= 24.9 && imc < 29.9){
-        _infoText = "Levemente Acima do Peso (${imc.toStringAsPrecision(4)})";
-      } else if(imc >= 29.9 && imc < 34.9){
-        _infoText = "Obesidade Grau I (${imc.toStringAsPrecision(4)})";
-      } else if(imc >= 34.9 && imc < 39.9){
-        _infoText = "Obesidade Grau II (${imc.toStringAsPrecision(4)})";
-      } else if(imc >= 40){
-        _infoText = "Obesidade Grau III (${imc.toStringAsPrecision(4)})";
+      double renda = double.parse(rendaControler.text);
+      double contrib = 0.0;
+      double descPrev = 0.0;
+      if (renda <= 998) {
+        contrib = _calcularValorIntegraldaFaixa(renda, 7.5);
+      } else {
+        contrib += _calcularValorIntegraldaFaixa(
+            998, 7.5); //faixa de 0 até 998 integralmente
+        if (renda > 2000) {
+          contrib += _calcularValorIntegraldaFaixa(
+              1001.99, 9.0); // faixa integral de 998 até 2000
+          if (renda > 3000) {
+            contrib += _calcularValorIntegraldaFaixa(
+                999.99, 12.0); //faixa integral de 2.000 ate 3.000
+            if (renda > 5839.45) {
+              contrib += _calcularValorIntegraldaFaixa(
+                  2839.44, 14.0); // faixa integral de 3000 até 5839.45
+              if (renda > 10000) {
+                contrib += _calcularValorIntegraldaFaixa(
+                    4160.54, 14.5); // faixa integral de 3000 até 5839.45
+                if (renda > 20000) {
+                  contrib += _calcularValorIntegraldaFaixa(9999.99, 16.5);
+                  if (renda > 39000) {
+                    contrib += _calcularValorIntegraldaFaixa(18999.99, 19);
+                    contrib += _calcularValorParcialdaFaixa(renda, 39000, 22);
+                  } else {
+                    contrib += _calcularValorParcialdaFaixa(renda, 20000, 19);
+                  }
+                } else {
+                  contrib += _calcularValorParcialdaFaixa(renda, 10000, 16.5);
+                }
+              } else {
+                contrib += _calcularValorParcialdaFaixa(renda, 5839.45, 14.5);
+              }
+            } else {
+              contrib += _calcularValorParcialdaFaixa(renda, 3000.0, 14.0);
+            }
+          } else {
+            contrib += _calcularValorParcialdaFaixa(renda, 2000.0, 12.0);
+          }
+        } else {
+          contrib += _calcularValorParcialdaFaixa(renda, 998.0, 9);
+        }
       }
+
+      print(contrib);
+      descPrev = contrib / renda * 100;
+      _infoText ="R\$(${contrib.toString().substring(0,contrib.toString().indexOf('.',0)+3)})";
+      _percet = "Desc. Prev: ${descPrev.toString().substring(0,descPrev.toString().indexOf('.',0)+2)}%";
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Calculadora de IMC"),
-        centerTitle: true,
-        backgroundColor: Colors.green,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.refresh),
-            onPressed: _resetFields,
-          )
-        ],
-      ),
-      backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Icon(Icons.person_outline, size: 120.0, color: Colors.green),
-              TextFormField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                    labelText: "Peso (kg)",
-                    labelStyle: TextStyle(color: Colors.green)),
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.green, fontSize: 25.0),
-                controller: weightController,
-                validator: (value) {
-                  if(value.isEmpty){
-                    return "Insira seu Peso!";
-                  }
-                },
-              ),
-              TextFormField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                    labelText: "Altura (cm)",
-                    labelStyle: TextStyle(color: Colors.green)),
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.green, fontSize: 25.0),
-                controller: heightController,
-                validator: (value) {
-                  if(value.isEmpty){
-                    return "Insira sua Altura!";
-                  }
-                },
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
-                child: Container(
-                  height: 50.0,
-                  child: RaisedButton(
-                    onPressed: () {
-                      if(_formKey.currentState.validate()){
-                        _calculate();
+        appBar: AppBar(
+          title: Text("Calculadora de Aliquota"),
+          centerTitle: true,
+          backgroundColor: Colors.white30,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: _resetFild,
+            )
+          ],
+        ),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          padding: EdgeInsets.all(10.0),
+          child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+
+                  Text("%",style: TextStyle(color: Colors.blueGrey, fontSize: 120.0),textAlign: TextAlign.center,),
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        labelText: "Renda (R\$)",
+                        labelStyle: TextStyle(color: Colors.blueGrey)),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.black, fontSize: 25.0),
+                    controller: rendaControler,
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return "O Valor é requerido";
                       }
                     },
-                    child: Text(
-                      "Calcular",
-                      style: TextStyle(color: Colors.white, fontSize: 25.0),
-                    ),
-                    color: Colors.green,
                   ),
-                ),
-              ),
-              Text(
-                _infoText,
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.green, fontSize: 25.0),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+                  Padding(
+                    padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                    child: Container(
+                      height: 50.0,
+                      child: RaisedButton(
+                        onPressed: () {
+                          if (_formKey.currentState.validate()) {
+                            _calcular();
+                          }
+                        },
+                        child: Text(
+                          "Calcular",
+                          style: TextStyle(color: Colors.white, fontSize: 25.0),
+                        ),
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    _infoText,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.blueGrey, fontSize: 25.0),
+                  ),
+                  Text(
+                    _percet,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.blueGrey, fontSize: 25.0),
+                  )
+                ],
+              )),
+        ));
   }
 }
